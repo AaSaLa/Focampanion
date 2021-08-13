60c7ba6a3b972f02f5e108134c7d9b85e7355a4f
Saksham Kumar <saksham.vvip@gmail.com>
2021-08-13 15:21:42 +0530

    Logo:tm:
    and made redirection quicker

--- js/content.js
+++ js/content.js
@@ -1,9 +1,7 @@
-window.onload = function(){
-    chrome.storage.sync.get("blocked_site", ({ blocked_site }) => {
-        if(blocked_site != ""){
-            if(window.location.hostname.includes(blocked_site)){
-                window.open("https://cl1p.net/pls%20study", "_self")
-            }
+chrome.storage.sync.get("blocked_site", ({ blocked_site }) => {
+    if(blocked_site != ""){
+        if(window.location.hostname.includes(blocked_site)){
+            window.open("https://cl1p.net/pls%20study", "_self")
         }
-    });
-}
+    }
+});
