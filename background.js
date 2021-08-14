5cefba02e1d7ae4b0e2c3419abc1f319e957a064
Saksham Kumar <saksham.vvip@gmail.com>
2021-08-14 14:05:10 +0530

    Added Remove Option

--- background.js
+++ background.js
@@ -2,6 +2,5 @@ let blocked_sites = [];
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({"blocked_sites": blocked_sites}, function(){
-        console.log("setted init blockedf_sites " + blocked_sites);
     });
 });
--- js/options.js
+++ js/options.js
@@ -9,3 +9,18 @@ document.getElementById("eyes").addEventListener("click", function(){
       console.log(result.blocked_sites);
   });
 });
+
+//TODO: Shift it elsewhere
+document.getElementById("remove_button").addEventListener("click", function(){
+    var remove_site = document.getElementById('blocked_site').value;
+
+    chrome.storage.sync.get(['blocked_sites'], function(result){
+        let blocked_sites = result.blocked_sites;
+        var remove_site_index = blocked_sites.indexOf(remove_site);
+        blocked_sites.splice(remove_site_index, 1);
+
+        chrome.storage.sync.set({"blocked_sites": blocked_sites}, function(){
+
+        });
+    });
+});
