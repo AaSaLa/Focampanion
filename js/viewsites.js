8ba490323347730c6a1a6bd65174a668aa503f66
Saksham Kumar <saksham.vvip@gmail.com>
2021-08-14 14:43:39 +0530

    dumb mistake

--- js/viewsites.js
+++ js/viewsites.js
@@ -1,6 +1,6 @@
 function display(){
     chrome.storage.sync.get(["blocked_sites"], function(result){
-        if(result.blocked_sites != 0){
+        if(result.blocked_sites.length != 0){
             document.getElementById("data").innerHTML = result.blocked_sites;
         }else{
             document.getElementById("data").innerText = "You haven't blocked anything :("
