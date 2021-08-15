1bd22bef3a80ba8e0795743e0b3a2ec8acc48af3
Aadish Saini <sainiaadish8@gmail.com>
2021-08-15 13:41:40 +0530

    Changed  interface
    nav with working items, removeSite a option now
    indisual pages for different options 
    better movement amongst pages
    .... and lot more

--- js/viewsites.js
+++ js/viewsites.js
@@ -1,29 +1,24 @@
-function display(){
-    var orderArray1 = ['mango', 'apple', 'orange'];
+function display(){ 
     chrome.storage.sync.get(['blocked_sites'], function(result) {
         var orderArray = result.blocked_sites;
+        numBlockedSite = orderArray.length;
         console.log(orderArray);
         var table = document.getElementById('table');
         var tbody = document.createElement('tbody');
         for (i = 0; i < orderArray.length; i++) {
             var row = document.createElement('tr');
             var cell = document.createElement('td');
-            var cell2 = document.createElement('td');
-            var img = new Image();
-            
-            img.src = 'images/cross.png';
-
             cell.className ='mt-4 text-light';
             cell.textContent = orderArray[i];
 
-            cell2.appendChild(img);
-
             row.appendChild(cell);
-            row.appendChild(cell2);
             tbody.appendChild(row);
         }
         table.appendChild(tbody);
     });
-    
 }
+document.getElementById('toRemovalPage').addEventListener('click', function toRemovalPage(){
+    window.location.href = 'extra/html/removalPage.html'
+  })
+
 window.onload = display;
