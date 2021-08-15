eb134783f7ea38dc64dbebc9bffd1dfe889ce0df
Aadish Saini <sainiaadish8@gmail.com>
2021-08-15 13:41:40 +0530

    Changed popup.html for new interface, nav with working items removeSite

---
+++ extra/html/removeBlockedSite.js
@@ -0,0 +1,3 @@
+document.getElementById('yes_button').addEventListener('click', function(){
+    window.location.href = 'confirmationPage.html'
+})
--- js/options.js
+++ js/options.js
@@ -11,17 +11,3 @@ document.getElementById("eyes").addEventListener("click", function(){
   document.getElementById('blocked_site').value = '';
 });
 
-document.getElementById("remove_button").addEventListener("click", function(){
-    var remove_site = document.getElementById('blocked_site').value;
-
-    chrome.storage.sync.get(['blocked_sites'], function(result){
-        let blocked_sites = result.blocked_sites;
-        var remove_site_index = blocked_sites.indexOf(remove_site);
-        blocked_sites.splice(remove_site_index, 1);
-
-        chrome.storage.sync.set({"blocked_sites": blocked_sites}, function(){
-
-        });
-    });
-    document.getElementById('blocked_site').value = '';
-});
