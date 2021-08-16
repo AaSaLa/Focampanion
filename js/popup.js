40a44fae8e796548f0ea6585f0607e73523babbb
Aadish Saini <sainiaadish8@gmail.com>
2021-08-16 11:22:47 +0530

    Code cleanup

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,9 @@
-document.getElementById("openNav").addEventListener("click", openNav);
-document.getElementById("closeNav").addEventListener("click", closeNav);
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+document.getElementById("openNav").addEventListener("click", openNav);
+document.getElementById("closeNav").addEventListener("click", closeNav);
