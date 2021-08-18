7b44a4638d3521d816af93d0a31b2405933baa68
Saksham Kumar <saksham.vvip@gmail.com>
2021-08-18 14:50:21 +0530

    TImer should WORKKKKkkkkkkkkkkkkk ahhhhhhhhhhhhhhhhhhh

--- js/content.js
+++ js/content.js
@@ -4,22 +4,23 @@ chrome.storage.sync.get(["blocked_sites"], function(result){
         let time = time_result.time;    
         for(var i = 0; i < blocked_sites.length; i++){
             var blocked_site = blocked_sites[i]
-            var tim = time[i];
-            var time_array = tim.split(":");
+            var tim = time[i].split("-")[0];
+            var block_time = time[i].split("-")[1];
+
             if(blocked_site != ""){
                 if(window.location.hostname.includes(blocked_site)){
-                    // setTimeout(() => {
-                    //     window.open("https://cl1p.net/pls%20study", "_self");
-                    // }, (parseInt(time_array[1]) * 1000) - time_array[0] * 1000);
+                    setTimeout(function(){
+                        window.open("https://cl1p.net/pls%20study", "_self");
+                        tim = tim.split(":")[1] + ":" + tim.split(":")[1];
+                        block_time = new Date.getTime();
+                    }, (parseInt(tim.split(":")[1]) * 60 * 1000) - tim.split(":")[0] * 60 * 1000);
 
-                    // setInterval(() => {
-                    //     tim = (parseInt(time_array[0]) + 1) + ":" + time_array[1];
-                    //     console.log(tim);
-                    //     time.splice(i, 0, tim)
-                    //     console.log(time);
-                    //     chrome.storage.sync.set({"time": time}, () => {});
-                    // }, 1000);
-                    window.open("https://cl1p.net/pls%20study", "_self");
+                    setInterval(() => {
+                        tim = (parseInt(tim.split(":")[0]) + 1) + ":" + tim.split(":")[1];
+                        time.splice(i, 0, tim)
+                        chrome.storage.sync.set({"time": time}, function(){
+                        });
+                    }, 60 * 1000);
                     return;
                 }
             }
