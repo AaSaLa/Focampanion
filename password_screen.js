23ce543ad0b9930d7357f05d48b7edc4e2ae6c24
Saksham Kumar <saksham.vvip@gmail.com>
2021-08-20 19:09:19 +0530

    Should be back to normal

--- password_screen.js
+++ password_screen.js
@@ -1,5 +1,12 @@
 document.getElementById('submit').addEventListener('click', function(){
     console.log("eyes");
     chrome.storage.sync.set({"password": document.getElementById("new_password").value});
-    window.location.href = 'options.html'
+    window.location.href = 'popup.html'
 })
+
+chrome.storage.sync.get(["password"], function(result){
+    if(result.password !== ""){
+        window.location.href = "popup.html";
+    }
+})
+
